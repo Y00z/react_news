4aa9405a239dc3ae20c02a0c82b96f8019af8a14
y00z <550823517@qq.com>
2017-05-15 16:00:27 +0800

    pc端登录注册

--- src/js/components/pc_header.js
+++ src/js/components/pc_header.js
@@ -4,25 +4,73 @@
 import React, {Component} from 'react';
 import 'antd/dist/antd.css'
 import logo from '../../image/logo.png';
-import {Row, Col} from 'antd';
-import {Menu, Icon} from 'antd';
+import {Menu, Icon, Modal, Button, Row, Col, Tabs, Form, Input, Checkbox} from 'antd';
+import {
+    BrowserRouter as Router,
+    Link
+} from 'react-router-dom'
+
+const TabPane = Tabs.TabPane
+const FormItem = Form.Item
 
 class PcHeader extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            current: 'toutiao'
+            current: 'toutiao',
+            logined: false,            //是否已经登录
+            username: null,
+            modalVisible: false,       //登录框是否显示
+            confirmDirty: false,        //注册密码确认
         }
+    }
+
+    setModalVisible = (value) => {
+        this.setState({
+            modalVisible: value
+        })
     }
 
     handleClick = (e) => {
         console.log('click ', e);
-        this.setState({
-            current: e.key,
+        if (e.key === "register") {
+            this.setModalVisible(true)
+        }else {
+            this.setState({
+                current: e.key,
+            });
+        }
+    }
+
+    callback = (key) => {
+        console.log(key)
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
         });
     }
 
     render() {
+        const {getFieldDecorator} = this.props.form;
+        const logined = this.state.logined
+            ?
+            <Menu.Item key="logout">
+                <Button type="primary" htmlType="button">{this.state.username}</Button>
+                &nbsp;&nbsp;
+                <Link>
+                    <Button type="dashed" htmlType="button">个人中学</Button>
+                </Link>
+                <Button type="primary" htmlType="button">退出</Button>
+            </Menu.Item>
+            :
+            <Menu.Item key="register">
+                <Icon type="appstore"/>注册/登录
+            </Menu.Item>
         return (
             <header>
                 <Row>
@@ -35,7 +83,7 @@ class PcHeader extends Component {
                     </Col>
                     <Col span={16}>
                         <Menu
-                            onClick={this.handleClick}
+                            onClick={(e) => this.handleClick(e)}
                             selectedKeys={[this.state.current]}
                             mode="horizontal">
                             <Menu.Item key="toutiao">
@@ -69,13 +117,116 @@ class PcHeader extends Component {
                             <Menu.Item key="shishang">
                                 <Icon type="appstore"/>时尚
                             </Menu.Item>
+                            {logined}
                         </Menu>
+                        <Modal title="Modal" visible={this.state.modalVisible}
+                               onOk={() => this.setModalVisible(false)} onCancel={() => this.setModalVisible(false)}
+                               okText="OK" cancelText="Cancel">
+                            <Tabs onChange={() => this.callback()} type="card">
+                                <TabPane tab="登录" key="1">
+                                    <Form onSubmit={(e) => this.handleSubmit(e)} className="login-form">
+                                        <FormItem>
+                                            {getFieldDecorator('nickname', {
+                                                rules: [{message: 'Please input your username!'}],
+                                            })(
+                                                <Input prefix={<Icon type="user" style={{fontSize: 13}}/>}
+                                                       placeholder="Username"/>
+                                            )}
+                                        </FormItem>
+                                        <FormItem>
+                                            {getFieldDecorator('password', {
+                                                rules: [{message: 'Please input your Password!'}],
+                                            })(
+                                                <Input prefix={<Icon type="lock" style={{fontSize: 13}}/>}
+                                                       type="password" placeholder="Password"/>
+                                            )}
+                                        </FormItem>
+                                        <FormItem>
+                                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                                登录
+                                            </Button>
+                                        </FormItem>
+                                    </Form>
+                                </TabPane>
+                                <TabPane tab="注册" key="2">
+                                    <Form onSubmit={(e) => this.handleSubmit(e)} className="login-form">
+                                        <FormItem
+                                            hasFeedback={true}
+                                        >
+                                            {getFieldDecorator('r_nickname', {
+                                                rules: [{
+                                                    message: 'The input is not valid username!',
+                                                }],
+                                            })(
+                                                <Input prefix={<Icon type="user" style={{fontSize: 13}}/>}
+                                                       placeholder="Username"/>
+                                            )}
+                                        </FormItem>
+                                        <FormItem
+                                            hasFeedback={true}
+                                        >
+                                            {getFieldDecorator('r_password', {
+                                                rules: [{
+                                                    message: 'Please input your password!',
+                                                }, {
+                                                    validator: this.checkConfirm,
+                                                }],
+                                            })(
+                                                <Input prefix={<Icon type="lock" style={{fontSize: 13}}/>}
+                                                       type="password" placeholder="Password"/>
+                                            )}
+                                        </FormItem>
+                                        <FormItem
+                                            hasFeedback={true}
+                                        >
+                                            {getFieldDecorator('r_confirm', {
+                                                rules: [{
+                                                    message: 'Please confirm your password!',
+                                                }, {
+                                                    validator: this.checkPassword,
+                                                }],
+                                            })(
+                                                <Input prefix={<Icon type="lock" style={{fontSize: 13}}/>}
+                                                       type="password" placeholder="Confirm Password"
+                                                       onBlur={(e) => this.handleConfirmBlur(e)}/>
+                                            )}
+                                        </FormItem>
+                                        <FormItem>
+                                            <Button type="primary" htmlType="submit" size="large">Register</Button>
+                                        </FormItem>
+                                    </Form>
+                                </TabPane>
+                            </Tabs>
+                        </Modal>
                     </Col>
                     <Col span={2}></Col>
                 </Row>
             </header>
         );
     }
+
+    checkPassword = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && value !== form.getFieldValue('r_password')) {
+            callback('Two passwords that you enter is inconsistent!');
+        } else {
+            callback();
+        }
+    }
+
+    checkConfirm = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && this.state.confirmDirty) {
+            form.validateFields(['r_confirm'], {force: true});
+        }
+        callback();
+    }
+
+    handleConfirmBlur = (e) => {
+        const value = e.target.value;
+        this.setState({confirmDirty: this.state.confirmDirty || !!value});
+    }
 }
 
-export default PcHeader;
+export default PcHeader = Form.create({})(PcHeader);
+;
